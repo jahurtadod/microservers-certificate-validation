870ffe40de100a0c3eb5c091e508d450537a4c96
leor0104 <luisortiz141999@gmail.com>
2021-02-15 18:35:04 -0500

    Post and Get Student

--- student-service/src/main/java/com/example/student/service/StudentServiceImpl.java
+++ student-service/src/main/java/com/example/student/service/StudentServiceImpl.java
@@ -5,9 +5,11 @@ import com.example.student.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
@@ -16,4 +18,35 @@ public class StudentServiceImpl implements StudentService {
     public Student getStudent(Long id) {
         return studentRepository.findById(id).orElse(null);
     }
+
+    @Override
+    public List<Student> findStudentAll() {
+        return studentRepository.findAll();
+    }
+
+    @Override
+    public Student findStudentByName(String name) {
+        return studentRepository.findByName(name);
+    }
+
+    @Override
+    public Student createStudent(Student student) {
+
+        student.setName("Luis Ortiz");
+        student.setEmail("leor@gmail.com");
+        return studentRepository.save(student);
+    }
+
+    @Override
+    public Student updateStudent(Student student) {
+        Student studentUpdate = getStudent(student.getId());
+        if (studentUpdate == null) {
+            return null;
+        }
+        studentUpdate.setName(student.getName());
+        studentUpdate.setEmail(student.getEmail());
+
+        return studentRepository.save(studentUpdate);
+    }
+
 }
