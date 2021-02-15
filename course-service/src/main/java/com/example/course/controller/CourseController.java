cc63693857b7c2abb731988193fdebbd4b1a7dec
Jorge Alcivar Hurtado Duarte <jahurtado11@utpl.edu.ec>
2021-02-15 11:01:36 -0500

    Read Configuration from the Config Server

--- course-service/src/main/java/com/example/course/controller/CourseController.java
+++ course-service/src/main/java/com/example/course/controller/CourseController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/coursers")
+@RequestMapping(value = "/courses")
 public class CourseController {
 
     @Autowired
