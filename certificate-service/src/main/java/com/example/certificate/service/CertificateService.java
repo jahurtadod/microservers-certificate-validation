f1fb2362452f5b84a629824762b9ea79caea8e3e
leor0104 <luisortiz141999@gmail.com>
2021-02-15 19:50:31 -0500

    Post, get and delete Certificate

--- certificate-service/src/main/java/com/example/certificate/service/CertificateService.java
+++ certificate-service/src/main/java/com/example/certificate/service/CertificateService.java
@@ -1,8 +1,17 @@
 package com.example.certificate.service;
 
 import com.example.certificate.entity.Certificate;
+import com.example.certificate.model.Student;
+import com.example.certificate.model.Course;
+
+import java.util.List;
 
 public interface CertificateService {
 
     Certificate getCertificate(Long id);
+    public List<Certificate> findCertificateAll();
+    public Certificate  createCertificate (Certificate certificate, Student student, Course course);
+    public Certificate updateCertificate (Certificate certificate, Student student, Course course);
+    public Certificate deleteCertificate (Certificate certificate);
+
 }
