32e4a3763350d1bbc6ceed763790b132d291557d
leor0104 <luisortiz141999@gmail.com>
2021-02-15 20:40:58 -0500

    Add methods HTTP Course

--- course-service/src/main/java/com/example/course/controller/CourseController.java
+++ course-service/src/main/java/com/example/course/controller/CourseController.java
@@ -8,6 +8,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
 
 @RestController
 @RequestMapping(value = "/courses")
@@ -22,6 +29,33 @@ public class CourseController {
         if (student == null)
             return ResponseEntity.notFound().build();
         return ResponseEntity.ok(student);
+    }
+    //-------------------------TODOS LOS CURSOS-------------------------
+    @GetMapping
+    public ResponseEntity<List<Course>> findCourseAll (@RequestParam(name = "Design UI", required = false) String name){
+        List<Course> courses = new ArrayList<>();
+        if (null == name) {
+            courses = courseService.findCourseAll();
+            if (courses.isEmpty()) {
+                return ResponseEntity.noContent().build();
+            }
+        }
+            return ResponseEntity.ok(courses);
+    }
+
+    @GetMapping(value = "/search")
+    public ResponseEntity<Course> getCourseBy(@RequestParam(name = "name", required = false) String name){
+        log.info("Buscando curso especcífico");
+        Course course = new Course();
+
+        if (name != null){
+            course = courseService.getCourseByName(name);
+            if (null == course){
+                log.error("Curso de nombre {} no encontrado", name);
+                return ResponseEntity.notFound().build();
+            }
+        }
+        return ResponseEntity.ok(course);
     }
 
 }
